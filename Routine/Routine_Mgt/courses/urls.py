79f22f2f672e0bbeb5d4cfa3dab4a5ead2059fea
Ummatun Khatun <ummatunkhatun086@gmail.com>
2025-04-25 21:03:29 +0600

    register and login

--- Routine/Routine_Mgt/courses/urls.py
+++ Routine/Routine_Mgt/courses/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('add/', views.add_course, name='add_course'),
+    path('register/', views.register, name='register'), 
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- Routine/Routine_Mgt/courses/views.py
+++ Routine/Routine_Mgt/courses/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Course
 from .forms import CourseForm  # You'll need to create this form
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib import messages
 def add_course(request):
     if request.method == 'POST':
         form = CourseForm(request.POST)
@@ -15,3 +17,37 @@ def add_course(request):
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'course/course_list.html', {'courses': courses})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful!")
+            return redirect('/')
+    else:  # Ensure form is initialized on a GET request
+        form = UserCreationForm()
+    
+    return render(request, 'courses/register.html', {'form': form})
+  
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, "Login successful!")
+            return redirect('/')
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'courses/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('/')
+
